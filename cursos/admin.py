9f1483c2d638fdfd37905f1389b724a322579012
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-13 20:02:01 -0300

    feat (admin): Registrando no admin

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from django.contrib import admin;
+from .models import Avaliacao, Cursos;
 
 # Register your models here.
+
+@admin.register(Cursos)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'url', 'criacao', 'atualizacao', 'ativo');
+
+@admin.register(Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ('curso', 'nome', 'email', 'comentario', 'avaliacao', 'criacao', 'atualizacao', 'ativo');
+
